f59196294f883a27b6a4e29d96672fe7dd532a5d
Daniel Pollock <danielstapleton@Daniels-MacBook-Pro.local>
2017-04-25 20:08:40 -0700

    classwork

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//Q1 is a sting
+//q2 is a number
+//q3 is a string array
+//q4 boolean
+//q6 array
+//q7 sting
+//q8 boolean
+//q9 mixed array
+//========4. Concatenation==
+//Hello my name is 'danny'
+//120
+//1010ten
+//5- if else/elseif
+//5.1 num4 is greater that 1
+//5.2 else statement ran
+//5.3 else if statement ran
+//5.4 else if statement ran
+// 6. for loop
+//thisisanarrayofstrings
+//25 but how??
+//11, 33, 55, 77, 99
+//functions
+// else, if, if
+// else, else if, else
